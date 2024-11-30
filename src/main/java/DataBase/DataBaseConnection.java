d1290a803ccb6c733c9a5075285c534caf012270
Tuchi504 <zepedajesus004@gmail.com>
2024-11-29 23:58:45 -0600

    Correciones para pull request #2

--- src/main/java/DataBase/DataBaseConnection.java
+++ src/main/java/DataBase/DataBaseConnection.java
@@ -17,9 +17,9 @@ public class DataBaseConnection {
 	
 	// Información de conexión a la base de datos
 	private static String dburl = "jdbc:sqlserver://localhost:1433;databaseName=HospitalDatabase;encrypt=true;trustServerCertificate=true";
-    private String dbUser;
-    private String dbPassword;
-    
+	private String dbUser;
+	private String dbPassword;
+	
     public DataBaseConnection(String dbUser, String dbPassword) {
     	
     	this.dbUser = dbUser;
@@ -28,7 +28,7 @@ public class DataBaseConnection {
     
 	/**
 	* Establece una conexion a la base de datos
-	* @author cdcruzr@unah.hn, jesus.zepeda@unah.hn
+	* @author cdcruzr@unah.hn
 	* @version 0.2.0
 	* @date 2024/11/15
 	* @since 2024/11/29
@@ -36,6 +36,6 @@ public class DataBaseConnection {
     public Connection getConnection() throws ClassNotFoundException, SQLException {
        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
        DriverManager.registerDriver(new SQLServerDriver());
-       return DriverManager.getConnection(dburl, this.dbUser, this.dbPassword);
+       return DriverManager.getConnection(dburl, dbUser, dbPassword);
    }
 }
