f18421684c0bc3692336b69f34e6c367fd9f324e
Blandin <almendarezbictor@gmail.com>
2024-11-29 23:09:15 -0600

    update 4

--- src/main/java/DataBase/DataBaseConnection.java
+++ src/main/java/DataBase/DataBaseConnection.java
@@ -4,38 +4,39 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.xml.crypto.Data;
+
 import com.microsoft.sqlserver.jdbc.SQLServerDriver;
 	/**
 	 * retorna la data una vez que se refresque la pantalla
 	 * @author cdcruzr@unah.hn
 	 * @version 0.1.0
 	 * @date 2024/11/15
-	 * @since 2024/11/15
+	 * @since 2024/11/29
 	 * */
 
 public class DataBaseConnection {
-	//Informacion de conexion a la base de datos
-	private static String dbUrl = "jdbc:sqlserver://localhost:1433;databaseName=loginUsers;encrypt=true;trustServerCertificate=true";
-	private String dbUer;
+	  // Información de conexión a la base de datos
+	private static String dburl = "jdbc:sqlserver://localhost:1433;databaseName=loginUsers;encrypt=true;trustServerCertificate=true";
+	private String dbUser;
 	private String dbPassword;
 	
-	public DataBaseConnection(String dbUser, String dbPassword) {
-		this.user = user;
-		this.password = password;
-		
-	}
-	
-
+    public DataBaseConnection(String dbUser, String dbPassword) {
+    	
+    	this.dbUser = dbUser;
+    	this.dbPassword = dbPassword;
+    }
+    
 	/**
 	* Establece una conexion a la base de datos
-	* @author cdcruzr@unah.hn, jesus.zepeda@unah.hn
+	* @author cdcruzr@unah.hn
 	* @version 0.1.0
 	* @date 2024/11/15
-	* @since 2024/11/15
+	* @since 2024/11/29
 	* */
-    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
        DriverManager.registerDriver(new SQLServerDriver());
-       return DriverManager.getConnection(dburl, this.dbUser, this.dbPassword);
+       return DriverManager.getConnection(dburl, dbUser, dbPassword);
    }
 }
